aac897f513ff2b5cbfd80e3763ce19175c177f31
Christian Lechner <lechnerc77@users.noreply.github.com>
2019-12-23 11:28:18 +0100

    POST and DELETE added

--- Counter/index.ts
+++ Counter/index.ts
@@ -4,8 +4,8 @@ module.exports = df.entity(function(context) {
     const currentValue = context.df.getState(() => 0);
     switch (context.df.operationName) {
         case "add":
-            const amount = context.df.getInput();
-            context.df.setState(currentValue + amount);
+            const inputValue = context.df.getInput();
+            context.df.setState(currentValue + inputValue);
             break;
         case "reset":
             context.df.setState(0);
--- HttpTrigger/index.ts
+++ HttpTrigger/index.ts
@@ -6,15 +6,36 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 
     const name = req.query.name;
     if (name) {
+
         const client = df.getClient(context);
         const entityId = new df.EntityId("Counter", req.query.name);
 
-        let entityState = await client.readEntityState(entityId);
-        await client.signalEntity(entityId, "add", 1);
+        if (req.method == "GET") {
 
-        context.res = {
-            body: "The current state value is " + entityState.entityState
-        };
+            let entityState = await client.readEntityState(entityId);
+
+            context.res = {
+                body: "The current state value of the entity " + req.query.name + " is " + entityState.entityState
+            };
+        }
+        else if (req.method == "POST") {
+
+            const increment: unknown = (req.query.increment ? req.query.increment : 1)
+            const incrementValue: number = Number(increment);
+
+            await client.signalEntity(entityId, "add", incrementValue);
+
+            context.res = {
+                body: "The current state value of the entity " + req.query.name + " was increased by " + incrementValue
+            };
+        }
+        else if (req.method == "DELETE") {
+            client.signalEntity(entityId, "reset");
+
+            context.res = {
+                body: "The state value of the entity " + req.query.name + " was reset to zero"
+            };
+        }
     }
     else {
         context.res = {
